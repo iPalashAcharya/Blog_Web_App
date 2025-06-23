f87980f8fc7a5ea3ba4015802493829065dfe488
iPalashAcharya <palashacharya2004@gmail.com>
2025-06-23 22:04:19 +0530

    final changes

--- db.js
+++ db.js
@@ -4,11 +4,7 @@ const { Pool } = require("pg");
 
 const connectionPool = new Pool({
     max: 5,
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
     }
