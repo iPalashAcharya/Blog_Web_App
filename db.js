31937e8f93cd05a7df0e109f75b0bf6bbd9f53fd
iPalashAcharya <palashacharya2004@gmail.com>
2025-06-23 22:41:43 +0530

    last try for connecting to database

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const { Pool } = require("pg");
 
 const connectionPool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: false, },
 });
 
 let poolEnded = false;
